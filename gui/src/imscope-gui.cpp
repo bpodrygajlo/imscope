7aaf67ad04fd32190c79210eb973c5d98e8eac9e
Bartosz Podrygajlo <bartosz.podrygajlo@openairinterface.org>
2026-05-04 22:07:36 +0200

    Drop AGPL license

--- gui/src/imscope-gui.cpp
+++ gui/src/imscope-gui.cpp
@@ -1,18 +1,8 @@
 /*
  * Copyright (c) 2025-2026 Bartosz Podrygajlo
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.
  */
 
 #include <stdio.h>
